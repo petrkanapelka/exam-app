3b0b268e2c9cf0fc64bcf2940d49d7186fc39523
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-13 22:05:12 +0300

    button update

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,10 +3,11 @@ type Props = {
     disable?: boolean
     className?: string
     onClick: () => void
+    test?: boolean
 };
 
 export const Button = ({ title, onClick, disable, className }: Props) => {
     return (
-        <button disabled={disable} onClick={onClick} className={className}>{title}</button>
+        <button disabled={disable} onClick={onClick} className={className}> {title} </button>
     )
 };
