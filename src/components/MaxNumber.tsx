644104f2a1f71dc9baea2cb405bed892cf41ba02
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-20 21:50:29 +0300

    refactor: delete unused

--- src/components/MaxNumber.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-    maxNumber: number
-};
-export const MaxNumberField = (props: Props) => {
-    return (
-        <div className="max-number">Max:{props.maxNumber} </div>
-    );
-};
