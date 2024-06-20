1fcce1398046a483e868e3b34417c990c58b2d54
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-21 01:16:59 +0300

    refactor: refactro Counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,20 +1,30 @@
-import { Controller } from '../controler/Controller';
-import { CounterTable } from '../counterTable/CounterTable';
+import { Controller } from './controler/Controller';
+import { CounterTable } from './counterTable/CounterTable';
 type CounterType = {
     count: number
+    minCount: number
     maxCount: number
     setCount: (value: number) => void
     setMaxCount: (value: number) => void
+    setMinCount: (value: number) => void
+    inputActive: boolean
 };
-export const Counter = ({ count, maxCount, setCount, setMaxCount }: CounterType) => {
+export const Counter = ({ count, maxCount, setCount, setMaxCount, minCount, setMinCount, inputActive }: CounterType) => {
     return (
         <div className='counter'>
-            <CounterTable count={count} maxCount={maxCount} />
+            <CounterTable
+                count={count}
+                maxCount={maxCount}
+                minCount={minCount}
+                inputActive={inputActive}
+            />
             <Controller
                 count={count}
                 setCount={setCount}
+                minCount={minCount}
                 maxCount={maxCount}
-                setMaxCount={setMaxCount} />
+                setMaxCount={setMaxCount}
+            />
         </div>
     );
 };
