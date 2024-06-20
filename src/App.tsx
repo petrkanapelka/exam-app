1560cf411eefb31c125c9f52dc78056a30a0bd35
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-21 01:16:24 +0300

    refactor: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import './App.css';
 import { useState } from 'react';
 import { Counter } from './components/counter/Counter';
+import { CounterSetup } from './components/сounterSetup/CounterSetup';
 
 function App() {
 
-  let [count, setCount] = useState(0);
+  let [minCount, setMinCount] = useState(0)
 
   let [maxCount, setMaxCount] = useState(5)
 
+  let [count, setCount] = useState(minCount);
+
+  let [inputActive, setInputActive] = useState(false)
+
+
+
   return (
     <div className="App">
-      <Counter count={count} setCount={setCount} maxCount={maxCount} setMaxCount={setMaxCount}/>
+      <CounterSetup
+        count={count}
+        setCount={setCount}
+        minCount={minCount}
+        setMinCount={setMinCount}
+        maxCount={maxCount}
+        setMaxCount={setMaxCount}
+        inputActive={inputActive}
+        setInputActive={setInputActive}
+      />
+      <Counter
+        count={count}
+        setCount={setCount}
+        minCount={minCount}
+        setMinCount={setMinCount}
+        maxCount={maxCount}
+        setMaxCount={setMaxCount}
+        inputActive={inputActive}
+      />
     </div>
   );
 }
