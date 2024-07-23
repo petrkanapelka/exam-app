45eb021b5df277d7ccb894fc788c4ada9486131b
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-23 21:46:37 +0300

    refactor: update CounterTable

--- src/components/counter/counterTable/CounterTable.tsx
+++ src/components/counter/counterTable/CounterTable.tsx
@@ -1,24 +1,30 @@
-type CountertableType = {
-    count: number
-    maxCount: number
-    minCount: number
-    inputActive: boolean
-    errorInput: boolean
-};
-export const CounterTable = ({ count, minCount, maxCount, inputActive, errorInput }: CountertableType) => {
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCount, selectMaxCount, selectMinCount, selectInputIsActive, selectInputIsError } from '../../modules/selectors/selectors';
+
+export const CounterTable: React.FC = React.memo(() => {
+    const count = useSelector(selectCount);
+    const maxCount = useSelector(selectMaxCount);
+    const minCount = useSelector(selectMinCount);
+    const inputIsActive = useSelector(selectInputIsActive);
+    const inputIsError = useSelector(selectInputIsError);
+
 
     const fullWidth = 100;
+    const progressWidth = (count - minCount) * (fullWidth / (maxCount - minCount));
 
     return (
         <div className="table-container">
             <div className={`table ${count === maxCount ? 'max' : ''}`}>
-                {!inputActive ?
-                    count :
-                    <span className={!errorInput ? 'active-span' : 'error-span'}>
-                        {errorInput ? 'incorrect input' : 'press set button'}
-                    </span>}
+                {!inputIsActive ? (
+                    count
+                ) : (
+                    <span className={!inputIsError ? 'active-span' : 'error-span'}>
+                        {inputIsError ? 'incorrect input' : 'press set button'}
+                    </span>
+                )}
             </div>
-            <div className="line" style={{ width: `${(count - minCount) * (fullWidth / (maxCount - minCount))}%` }}></div>
+            <div className="line" style={{ width: `${progressWidth}%` }}></div>
         </div>
     );
-};
+});
