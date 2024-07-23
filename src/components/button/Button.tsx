2721ebcdac491a7044744e4c8820051ebc51f7bd
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-23 21:46:00 +0300

    refactor: update Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import { ButtonHTMLAttributes } from "react";
 
 type ButtonType = ButtonHTMLAttributes<HTMLButtonElement> & {
     disable?: boolean
 };
 
-export const Button = ({ title, onClick, disable, className }: ButtonType) => {
+export const Button = React.memo(({ title, onClick, disable, className }: ButtonType) => {
     return (
         <button disabled={disable} onClick={onClick} className={className}> {title} </button>
     )
-};
+});
