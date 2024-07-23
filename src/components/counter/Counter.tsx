44611e1bacccaabaef3bc20cbb7f1e0c9e352a16
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-23 21:46:09 +0300

    refactor: update Counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,41 +1,12 @@
+import React from 'react';
 import { Controller } from './controler/Controller';
 import { CounterTable } from './counterTable/CounterTable';
-type CounterType = {
-    count: number
-    minCount: number
-    maxCount: number
-    setCount: (value: number) => void
-    setMaxCount: (value: number) => void
-    setMinCount: (value: number) => void
-    inputActive: boolean
-    errorInput: boolean
-};
-export const Counter = ({
-    count,
-    maxCount,
-    setCount,
-    setMaxCount,
-    minCount,
-    setMinCount,
-    inputActive,
-    errorInput
- }: CounterType) => {
+
+export const Counter = React.memo(() => {
     return (
         <div className='counter'>
-            <CounterTable
-                count={count}
-                maxCount={maxCount}
-                minCount={minCount}
-                inputActive={inputActive}
-                errorInput={errorInput}
-            />
-            <Controller
-                count={count}
-                setCount={setCount}
-                minCount={minCount}
-                maxCount={maxCount}
-                setMaxCount={setMaxCount}
-            />
+            <CounterTable />
+            <Controller />
         </div>
     );
-};
+});
