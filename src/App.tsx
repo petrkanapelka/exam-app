c96d95f3d70fecf20909bfe2971a9ab2e817212f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-23 21:45:35 +0300

    refactor: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
 import './App.css';
-import { useState } from 'react';
 import { Counter } from './components/counter/Counter';
 import { CounterSetup } from './components/сounterSetup/CounterSetup';
 
 function App() {
-
-  const initialMinCount = Number(localStorage.getItem('minCount')) || 0;
-  const initialMaxCount = Number(localStorage.getItem('maxCount')) || 5;
-  const initialCount = Number(localStorage.getItem('count')) || initialMinCount;
-
-  let [minCount, setMinCount] = useState(initialMinCount);
-  let [maxCount, setMaxCount] = useState(initialMaxCount);
-  let [count, setCount] = useState(initialCount);
-  let [inputActive, setInputActive] = useState(false);
-  let [errorInput, setErrorInput] = useState(false);
-
   return (
     <div className="App">
-      <CounterSetup
-        count={count}
-        setCount={setCount}
-        minCount={minCount}
-        setMinCount={setMinCount}
-        maxCount={maxCount}
-        setMaxCount={setMaxCount}
-        inputActive={inputActive}
-        setInputActive={setInputActive}
-        setErrorInput={setErrorInput}
-        errorInput={errorInput}
-      />
-      <Counter
-        count={count}
-        setCount={setCount}
-        minCount={minCount}
-        setMinCount={setMinCount}
-        maxCount={maxCount}
-        setMaxCount={setMaxCount}
-        inputActive={inputActive}
-        errorInput={errorInput}
-      />
+      <CounterSetup />
+      <Counter />
     </div>
   );
 }
